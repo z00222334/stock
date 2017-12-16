29567a630ed2e0e1bd4f16193bee1355cb4e7696
zero02358 <377046838@qq.com>
2017-12-16 15:15:20 +0800

    修正：使用日期作为新数据的判断，表中的index值不具备参考价值

--- InsertToMySQL.py
+++ InsertToMySQL.py
@@ -12,18 +12,22 @@ cur = conn.cursor()
 engine = create_engine('mysql://root:root@127.0.0.1/stock_info?charset=utf8')
 
 for each_stock_id in all_stock_id:
+    print (each_stock_id)
     df = ts.get_k_data(each_stock_id)
-    str_sql = "SELECT max(k_data.index) FROM k_data WHERE k_data.code ='" + each_stock_id + "'"
+    str_sql = "SELECT max(k_data.date) FROM k_data WHERE k_data.code ='" + each_stock_id + "'"
     try:
         cur.execute(str_sql)
     except:  # 表不存在的情况，下载所有数据插入表
         df = ts.get_k_data(each_stock_id)
         df.to_sql('k_data', engine, if_exists='append')
 
-    new_index = ts.get_k_data(each_stock_id).tail(1).index.max()  # 最近交易日的index值
-    db_max_index = cur.fetchone()[0]        # db中存在的最大index值
-    insert_days = int(new_index) - int(db_max_index)
-    df.tail(insert_days).to_sql('k_data', engine, if_exists='append') # 将最新数据插入数据库
-
+    date = ts.get_k_data(each_stock_id).tail(1).date.max()  # 最近交易日的日期
+    db_max_date = cur.fetchone()[0]            # db中存在的最大date值
+    if db_max_date is None:                   # 如果db中不存在相应的code数据，则直接把所有数据插入数据库
+        df.to_sql('k_data', engine, if_exists='append')
+    else:
+        for x in range(1,100):               #循环遍历，找出db中的最大日期
+            if ts.get_k_data(each_stock_id).tail(x).date.min() == db_max_date:
+                df.tail(x-1).to_sql('k_data', engine, if_exists='append')  # 将最新数据插入数据库
 
 
