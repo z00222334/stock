365f8c3102851571842dae8e4e529ace0a7a2107
z00222334 <wudihuoyan@qq.com>
2017-12-27 08:20:52 +0800

    补充code map获取的日志

--- codemap.py
+++ codemap.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import tushare as ts
+import logging
 import pandas as pd
 
 import os
@@ -17,12 +18,13 @@ def generate_map():
         for i in range(len(ret)):
             # print type(namelist[i])
             # 这里发现namelist的元素都是Unicode的，不是str因此需要转换，转换就编码成utf-8吧，方便点。
-            f.writelines(str(codelist[i]) + "," + namelist[i].encode("utf-8") + "\n")
-
+            if "N" not in str(codelist[i]): # 如果不是新股上市 那就写入
+                f.writelines(str(codelist[i]) + "," + namelist[i].encode("utf-8") + "\n")
+    logging.debug("get code and name map end.")
 
 if __name__ == '__main__':
-    # generate_map()
+    generate_map()
 
-    print pd.read_csv(codemap_file).set_index("code")
-    s=pd.read_csv(codemap_file).set_index("code")
-    print s.ix[603283]["name"]
+    # print pd.read_csv(codemap_file).set_index("code")
+    # s=pd.read_csv(codemap_file).set_index("code")
+    # print s.ix[603283]["name"]
